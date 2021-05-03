59753255b01485fa98c60292a12968dbd7850f7c
kaerunrun <take.t.public@gmail.com>
2021-05-03 10:09:04 +0900

    なんか見た目が面白いので

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,23 +1,34 @@
 <template>
-  <div>
+  <v-form name="main">
     <!-- 説明の表示 -->
     <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
+      <v-col cols="12">
         <p>さあ、出勤しよう</p>
       </v-col>
     </v-row>
+    <!-- 社員番号入力 -->
+    <v-text-field label="社員番号" outlined />
     <!-- ボタンの配置 -->
     <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-btn color="primary">
+      <v-col cols="12" sm="6">
+        <v-btn color="primary" rounded>
           <span>出勤する</span>
         </v-btn>
       </v-col>
+      <v-col cols="12" sm="6">
+        <v-btn color="error" rounded>
+          <span>退勤する</span>
+        </v-btn>
+      </v-col>
     </v-row>
-  </div>
+  </v-form>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+* {
+  //outline: 1px solid lime;
+}
+
 p {
   font-family: 'Shippori Mincho';
 }
