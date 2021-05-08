8be36a7b15ae7b60db63640947346c79f7d4718b
kaerunrun <take.t.public@gmail.com>
2021-05-09 01:06:12 +0900

    sampleコピペ

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
     <v-app-bar app dense>
-      <p class="class_title">ハンコつくる</p>
+      <v-toolbar-title class="class_title">
+        <span>ハンコつくる</span>
+      </v-toolbar-title>
+      <v-tabs>
+        <v-tab to="/">top</v-tab>
+        <v-tab to="transformer">transformer</v-tab>
+      </v-tabs>
     </v-app-bar>
     <v-main>
       <v-container>
-        <nuxt />
+        <Nuxt />
       </v-container>
     </v-main>
     <v-footer app>
@@ -14,10 +20,12 @@
   </v-app>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .class_title {
   font-family: 'Shippori Mincho';
   margin: 0;
   font-size: xx-large;
+  overflow: visible;
+  text-overflow: unset;
 }
 </style>
